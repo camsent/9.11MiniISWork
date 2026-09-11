6025bf556d95242ce7ea422ae2c63706c9c70268
camsent <csentieri27@wooster.edu>
2026-09-11 11:20:50 -0400

    added the serial connection in Python and made a while loop to give it a real time diagnostic feel

--- python/diagnosis.py
+++ python/diagnosis.py
@@ -2,16 +2,27 @@
 # a range of voltages from 2.4-2.6 will be considered normal.
 # close to 0V means the resistor is disconnected from Power
 # close to 5V means the resistor is disconnected from GND
+import serial
 
-voltage = float(input("Enter the voltage reading from the resistor: "))
+arduino = serial.Serial("COM5", 9600)
 
-if voltage < 1.5:
-    print("\nThe resistor is disconnected from Power")
-elif 1.5 <= voltage < 2.4:
-    print("\nabnormal behavior, unknown fault")
-elif 2.6 < voltage <= 3.7:
-    print("\nabnormal behavior, unknown fault")
-elif voltage > 3.7:
-    print("\nThe resistor is disconnected from GND")
-else:
-    print("\nThe resistor is connected properly")
+while True:
+
+    voltage = float(arduino.readline().decode().rstrip())
+
+
+    print(voltage)
+
+    def diagnose_voltage(voltage): 
+        if voltage < 1.5:
+            print("\nThe resistor is disconnected from Power")
+        elif 1.5 <= voltage < 2.4:
+            print("\nabnormal behavior, unknown fault")
+        elif 2.6 < voltage <= 3.7:
+            print("\nabnormal behavior, unknown fault")
+        elif voltage > 3.7:
+            print("\nThe resistor is disconnected from GND")
+        else:
+            print("\nThe resistor is connected properly")
+
+    diagnose_voltage(voltage)
