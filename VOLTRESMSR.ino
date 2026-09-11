7f60bf1d9b0fb159e943352851550be7c65c848e
Cam Sentieri <csentieri27@wooster.edu>
2026-09-11 09:38:29 -0400

    adding file to repo to be worked on from LT

---
+++ VOLTRESMSR.ino
@@ -0,0 +1,19 @@
+//the idea behind this sketch is to figure how to measure voltage across a resistor
+//when res is dc'd from GND V = 5
+//when res is dc'd from POS V = 0
+//normal behavior is V = 2.5
+int testPoint = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int testVal = analogRead(testPoint);
+  float voltageVal = testVal * (5.0/1023.0);
+  Serial.print(voltageVal);
+  Serial.println(" V");
+  delay(500);
+}
